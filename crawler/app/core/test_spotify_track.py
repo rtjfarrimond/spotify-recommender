e874b9e930012876c719b7438cd12442d1cf831b
Robert Farrimond <rtf676@cs.bham.ac.uk>
2019-01-24 07:06:50 +0000

    Finish SpotifyTrack tests.

--- crawler/app/core/test_spotify_track.py
+++ crawler/app/core/test_spotify_track.py
@@ -11,9 +11,9 @@ logger = logging.getLogger(__name__)
 def make_track(
         spotify_id='dummy',
         name='dummy',
-        artists='dummy',
+        artists=[],
         preview_url='dummy'):
-    return SpotifyTrack({
+    return SpotifyTrack.from_json({
         'id': spotify_id,
         'name': name,
         'artists': artists,
@@ -21,7 +21,20 @@ def make_track(
         })
 
 
+def artist_strings(list_artists):
+    return [str(a) for a in list_artists]
+
+
 class TestSpotifyTrack(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.spotify_id = "dummy_id"
+        cls.name = "track title"
+        cls.artist_1 = {"name": "artist_1"}
+        cls.artist_2 = {"name": "artist_2"}
+        cls.artists = [cls.artist_1, cls.artist_2]
+        cls.preview_url = "dummy_url"
 
     def test_default_constructor(self):
         attrs = [('attr_1', 'a'), ('attr_2', 'b')]
@@ -37,22 +50,18 @@ class TestSpotifyTrack(unittest.TestCase):
         self.assertIsNone(track.preview_url)
 
     def test_from_json_contructor(self):
-        def artist_strings(list_artists):
-            return [str(a) for a in list_artists]
-
-        artists = [{'name': 'artist_1'}, {'name': 'artist_2'}]
-        spotify_artists = [SpotifyArtist(name) for name in artists]
-        d = {'id': 'dummy_id',
-             'name': 'dummy_name',
-             'artists': artists,
-             'preview_url': 'dummy_url'}
-        track = SpotifyTrack.from_json(d)
-        self.assertEqual(d['id'], track.spotify_id)
-        self.assertEqual(d['name'], track.name)
+        spotify_artists = [SpotifyArtist(name) for name in self.artists]
+        track = make_track(
+            spotify_id=self.spotify_id,
+            name=self.name,
+            artists=self.artists,
+            preview_url=self.preview_url)
+        self.assertEqual(self.spotify_id, track.spotify_id)
+        self.assertEqual(self.name, track.name)
         self.assertEqual(
                 artist_strings(spotify_artists),
                 artist_strings(track.artists))
-        self.assertEqual(d['preview_url'], track.preview_url)
+        self.assertEqual(self.preview_url, track.preview_url)
 
     def test_download_preview_value_error_when_no_preview_url(self):
         track = SpotifyTrack({})
@@ -63,27 +72,17 @@ class TestSpotifyTrack(unittest.TestCase):
         self.assertEqual('', track.get_artists_string())
 
     def test_get_artists_string_formatting(self):
-        artist_1 = 'artist_1'
-        artist_2 = 'artist_2'
-        expected = f'{artist_1}, {artist_2}'
-        track = SpotifyTrack.from_json(
-                {'id': 'dummy',
-                 'name': 'dummy',
-                 'artists': [{'name': artist_1}, {'name': artist_2}],
-                 'preview_url': 'dummy'})
+        expected = f"{self.artist_1['name']}, {self.artist_2['name']}"
+        track = make_track(artists=[self.artist_1, self.artist_2])
         self.assertEqual(expected, track.get_artists_string())
 
     def test_repr_formatting(self):
-        artist_1 = 'artist_1'
-        artist_2 = 'artist_2'
-        name = 'track title'
-        expected = f'{artist_1}, {artist_2} - {name}'
-        track = SpotifyTrack.from_json(
-                {'id': 'dummy',
-                 'name': name,
-                 'artists': [{'name': artist_1}, {'name': artist_2}],
-                 'preview_url': 'dummy'})
+        expected = f"{self.artist_1['name']}, " + \
+                   f"{self.artist_2['name']} - {self.name}"
+        track = make_track(
+            name=self.name, artists=[self.artist_1, self.artist_2])
         self.assertEqual(expected, track.__repr__())
 
+
 if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite)
+    unittest.main()
