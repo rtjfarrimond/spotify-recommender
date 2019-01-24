077508a36485d83bf18ed4fb402c4c69c620549d
Robert Farrimond <rtf676@cs.bham.ac.uk>
2019-01-24 07:48:40 +0000

    Add template for tests.
    - Added template to test PlaylistCrawler.
    - All tests in class currently force fail.

---
+++ crawler/app/core/test_playlist_crawler.py
@@ -0,0 +1,44 @@
+import unittest
+from core.spotify_track import SpotifyTrack
+
+
+class MockSpotifyTrack(SpotifyTrack):
+
+    def __init__(self):
+        pass
+
+
+class TestPlaylistCrawler(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    def test_default_constructor_happy_path(self):
+        self.assertTrue(False)
+
+    def test_default_constructor_value_error_when_username_null(self):
+        self.assertTrue(False)
+
+    def test_default_constructor_value_error_when_playlist_url_null(self):
+        self.assertTrue(False)
+
+    def test_default_constructor_trims_trailing_slash_from_playlist_url(self):
+        self.assertTrue(False)
+
+    def test_parse_json_single_page(self):
+        self.assertTrue(False)
+
+    def test_parse_json_multiple_pages(self):
+        self.assertTrue(False)
+
+    def test_download_previews(self):
+        # Mock SpotifyTrack for this
+        self.assertTrue(False)
+
+    def test_repr(self):
+        self.assertTrue(False)
+
+
+if __name__ == '__main__':
+    unittest.main()
