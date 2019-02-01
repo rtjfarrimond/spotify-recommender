6219dcbdaf2902d490ae4349a271a90a0cbf3843
Robert Farrimond <rtf676@cs.bham.ac.uk>
2019-02-01 06:35:18 +0000

    Add more Terraform code.

--- infrastructure/terraform/config.tf
+++ infrastructure/terraform/config.tf
@@ -7,12 +7,20 @@ resource "aws_s3_bucket" "example" {
   acl = "private"
 }
 
+output "s3_domain" {
+  value = "${aws_s3_bucket.example.bucket_domain_name}"
+}
+
 resource "aws_instance" "example" {
-  ami = "ami-0e12cbde3e77cbb98"
+  ami = "${lookup(var.amis, var.region)}"
   instance_type = "t2.micro"
   depends_on =["aws_s3_bucket.example"]
 }
 
 resource "aws_eip" "ip" {
   instance = "${aws_instance.example.id}"
+}
+
+output "ip" {
+  value = "${aws_eip.ip.public_ip}"
 }
--- infrastructure/terraform/variables.tf
+++ infrastructure/terraform/variables.tf
@@ -1,3 +1,11 @@
 variable "region" {
   default = "eu-west-1"
 }
+
+variable "amis" {
+  type = "map"
+  default = {
+    "eu-west-1" = "ami-0e12cbde3e77cbb98"
+    "us-west-1" = "ami-063aa838bd7631e0b"
+  }
+}
