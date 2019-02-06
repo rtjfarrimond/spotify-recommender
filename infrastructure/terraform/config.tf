6f9299c9725a3a42904c439d6d3011d92ee9ad77
Robert Farrimond <rtf676@cs.bham.ac.uk>
2019-02-06 09:20:38 +0000

    Terraform remote state to s3.

--- infrastructure/terraform/config.tf
+++ infrastructure/terraform/config.tf
@@ -1,11 +1,23 @@
+terraform {
+  backend "s3" {
+    bucket = "spotify-recommender-bucket"
+    key    = "terraform.tfstate"
+    region = "eu-west-1"
+  }
+}
+
 provider "aws" {
   region = "${var.region}"
 }
 
-resource "aws_s3_bucket" "recommender-bucker" {
+resource "aws_s3_bucket" "recommender-bucket" {
   bucket = "spotify-recommender-bucket"
   acl    = "private"
   region = "${var.region}"
+
+  versioning {
+    enabled = true
+  }
 }
 
 resource "aws_dynamodb_table" "audio-features" {
