fe479dab1ca5c7b31e2ea06496e2b44a8ff60c2c
Robert Farrimond <rtf676@cs.bham.ac.uk>
2019-03-17 11:18:15 +0000

    Reduce memory consumption to constant, rather than linear w.r.t. input size.

--- extractor/app/app.py
+++ extractor/app/app.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 from argparse import Namespace
 from core.models_transfer import build_convnet_model
+from functools import partial
 from keras import backend as K
 from zipfile import ZipFile
 import numpy as np
@@ -69,17 +70,15 @@ def load_audio(audio_path):
         return src[np.newaxis, np.newaxis, :ref_n_src]
 
 
-def _paths_models_generator(lines, models):
+def _paths_models_generator(lines):
     for line in lines:
-        yield (line.rstrip('\n'), models)
+        yield (line.rstrip('\n'), MODELS)
 
 
-def _predict_one(args):
-    """target function in pool.map()"""
-    line, models = args
+def _predict_one(line):
     audio_path = line.rstrip('\n')
     src = load_audio(audio_path)
-    features = [models[i].predict(src)[0] for i in range(5)]
+    features = [MODELS[i].predict(src)[0] for i in range(5)]
     return np.concatenate(features, axis=0)
 
 
@@ -106,45 +105,39 @@ def empty_safe_concat_array(could_be_empty, to_concat):
             [could_be_empty, np.array(to_concat, dtype=np.float32)])
 
 
-def predict_cpu(f_path, models, n_jobs):
-    """ Predict features with multiprocessing.
+def extract_features(table_name, region, f_path, track):
+    def write_to_db(track, features, table_name, region):
+        logger.info(
+            "Writing track {} to DynamoDB Table {}...".format(
+                track,
+                table_name
+            )
+        )
+        track = track['track']
+        track_id = track['id']
 
-    Parameters:
-    -----------
-    f_path:
-        string, path to the input audio file.
+        # TODO: Pull hash and sort keys from ssm.
+        item = {
+            "TrackId": track_id,
+            "Source": "spotify",
+            "Title": track['title'],
+            "Artists": track['artists'],
+            "PreviewUrl": track['preview_url'],
+            "Features": features
+        }
+        dynamodb = boto3.resource('dynamodb', region_name=region)
+        table = dynamodb.Table(table_name)
+        table.put_item(Item=item)
 
-    models:
-        list of keras models to use for feature extraction.
+    logger.info(
+        "Extracting features for track {}...".format(
+            track['track']['id']
+        )
+    )
+    features = _predict_one(f_path)
+    pkl_features = pickle.dumps(features, protocol=0)
 
-    n_jobs:
-        int, number of threads to use.
-
-    Returns:
-    --------
-        numpy.array, features extracted.
-    """
-    pool = multiprocessing.Pool(processes=n_jobs)
-    paths = [f_path]
-    ret = np.array([])
-
-    arg_gen = _paths_models_generator(paths[:], models)
-    features = pool.map(_predict_one, arg_gen)
-    return empty_safe_concat_array(ret, features)
-
-
-def extract_features(paths, out_path, n_jobs=1):
-    models = [load_model(mid_idx) for mid_idx in range(5)]
-    count = 1
-    all_features = []
-    for f_path in paths:
-        logger.info("Extracting features from track {} of {}...".format(
-                    count, len(paths)))
-        count += 1
-        features = predict_cpu(f_path, models, n_jobs)
-        all_features.append(pickle.dumps(features, protocol=0))
-
-    return all_features
+    write_to_db(track, pkl_features, table_name, region)
 
 
 def download_extract_zip(bucket, zip_name, dir_name='/tmp'):
@@ -186,25 +179,6 @@ def parse_tracks_json_lines(json_lines):
     return tracks
 
 
-def write_to_db(tracks, all_features, table_name, region="eu-west-1"):
-    logger.info("Writing items to DynamoDB Table {}...".format(table_name))
-    for i in range(len(all_features)):
-        track = tracks[i]['track']
-        track_id = track['id']
-
-        item = {
-            "TrackId": track_id,
-            "Source": "spotify",
-            "Title": track['title'],
-            "Artists": track['artists'],
-            "PreviewUrl": track['preview_url'],
-            "Features": all_features[i]
-        }
-        dynamodb = boto3.resource('dynamodb', region_name=region)
-        table = dynamodb.Table(table_name)
-        table.put_item(Item=item)
-
-
 if __name__ == '__main__':
     DYNAMODB_TABLE = settings.DYNAMODB_TABLE
     S3_BUCKET_NAME = settings.S3_BUCKET_NAME
@@ -236,14 +210,31 @@ if __name__ == '__main__':
     # Download the zip file.
     download_extract_zip(S3_BUCKET_NAME, ZIP_FILE_NAME, dir_name=audio_path)
 
+    # Load models
+    logger.info("Loading models...")
+    MODELS = [load_model(mid_idx) for mid_idx in range(5)]
+
     # Extract audio features
     paths = glob.glob('{}/**/*.mp3'.format(audio_path))
     json_lines = glob.glob('{}/**/*.json'.format(audio_path))[0]
     tracks = parse_tracks_json_lines(json_lines)
-    features = extract_features(paths, feature_path)
+    pa_extract = partial(
+        extract_features,
+        table_name=DYNAMODB_TABLE,
+        region=AWS_DEFAULT_REGION
+    )
 
-    # Write to DynamoDB
-    write_to_db(tracks, features, DYNAMODB_TABLE, region=AWS_DEFAULT_REGION)
+    # Extract and upload features.
+    count = 1
+    for i in range(len(paths)):
+        logger.info(
+            "Extracting features for track {} of {}...".format(
+                count,
+                len(paths)
+            )
+        )
+        count += 1
+        pa_extract(f_path=paths[i], track=tracks[i])
 
     # Delete the zip from s3.
     delete_zip(S3_BUCKET_NAME, ZIP_FILE_NAME)
